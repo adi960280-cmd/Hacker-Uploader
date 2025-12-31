7d444bc2c65d74ec810f0fc31b30a419e5b93da6
adi960280-cmd <adityamalhotra960@gmail.com>
2025-12-31 21:08:46 +0530

    Remove default BOT_TOKEN from vars.py
    Remove default BOT_TOKEN value for security reasons.

--- vars.py
+++ vars.py
@@ -2,4 +2,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29115102"))
 API_HASH = environ.get("API_HASH", "1a331db2b00e9d2decaa9c7276449eb6")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7903742155:AAGLWumrUPxZpl0yakb4oDhfuq74wtWRzdY")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
